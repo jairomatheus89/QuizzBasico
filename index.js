315c4bd055d5c344483eb1f3ac4bbe3ae8084575
Jairopln <87547016+jairomatheus89@users.noreply.github.com>
2024-10-26 19:10:15 -0300

    calculatorChange
    trocando projeto por calculadora

--- index.js
+++ index.js
@@ -1,53 +1,41 @@
-//boas vindas e Start
-var nickvulgo = window.prompt("Qual teu vulgo, otário?");
-document.getElementById('userName').innerHTML = nickvulgo.toLowerCase();
+const buttons = [
+    '7','8','9','/',
+    '4','5','6','+',
+    '1','2','3','-',
+    'C','0','*','=',
+]
 
-function StartQuizz(){
-    document.getElementById('quizzBox').style.visibility = 'visible';
-}
+const calculator = document.getElementById("calculatorDiv");
 
+const inputDisplay = document.getElementById("displayRes");
 
+let calculatorResult = document.getElementById("CalculatorResult");
 
+buttons.forEach(value => {
+    const button = document.createElement("buttons");
+    button.innerText = value;
+    button.classList.add("buttons");
 
-//QUESTIONs
+    button.addEventListener("click", () => {
+        console.log("buttonclicked: ",value);   
+        if(value === "="){
 
-const questions = [
-    {question: 'Qual o resultado da expressão (2 + 5):', answer:'7'},
-    {question: 'Okay sabixão, Agora me diga qual o resultado da expressão (4 * 2):', answer:'8'},
-    {question: 'Qual é a capital da França?', answer:'paris'}
-];
+            let displayExp = inputDisplay.innerText;
+            let result = eval(displayExp);
 
-let currentQuestionIndex = 0;
+            console.log(`The result of expression is: ${result}`);
+            calculatorResult.innerText = `The result is: ${result}`;
+            
+            inputDisplay.innerText = "";
 
-const divQuizz = document.getElementById('quizzBox');
-
-const btnResp = document.getElementById('respbut');
-const input = document.getElementById('resp');
-const questionElement = document.getElementById('question');
-const questionCountElement = document.getElementById('questionCount');
-const failureDiv = document.getElementById('failureDiv');
-
-function checkAnswer() {
-    const userAnswer = input.value.toLowerCase();
-    if (userAnswer === questions[currentQuestionIndex].answer) {
-        currentQuestionIndex ++;
-        if (currentQuestionIndex < questions.length) {
-            questionElement.innerText = questions[currentQuestionIndex].question;
-            questionCountElement.innerHTML = `Question: ${currentQuestionIndex + 1} de ${questions.length}`;
+        }else if (value === "C") {
+            console.log("cleaning Display!");
+            inputDisplay.innerText = "";
         } else {
-            location.href = "jumpscare.html";
+            inputDisplay.innerText += value;
         }
 
-    } else {
-        divQuizz.innerHTML = 'errado Otário... clique em "restart" para reiniciar'
-        failureDiv.style.visibility = 'visible';
-    }
-    input.value = '';
-}
+    })
 
-
-btnResp.addEventListener('click', checkAnswer);
-
-function restartPage(){
-    window.location.reload();
-}
+    calculator.appendChild(button);
+});
--- jumpscare.js
+++
@@ -1,8 +0,0 @@
-function clickReveal() {
-    const audio = document.getElementById('audioscare');
-
-    document.getElementById('premioAnnounce').style.visibility = 'hidden';
-    audio.play();
-
-
-}
